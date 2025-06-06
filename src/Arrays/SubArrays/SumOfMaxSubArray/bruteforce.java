482da6c0ebff34eb2c62128f2b9d7fa6f7b59602
007Edge <edge07.shiv@gmail.com>
2025-06-06 21:45:55 +0530

    max subarray sum

---
+++ src/Arrays/SubArrays/SumOfMaxSubArray/bruteforce.java
@@ -0,0 +1,25 @@
+package Arrays.SubArrays.SumOfMaxSubArray;
+
+public class bruteforce {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 6, -1, 3};
+        int max = sumSA(arr);
+        System.out.println(max);
+    }
+
+    public static int sumSA(int[] arr) {
+        int max = -100000;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i; j<arr.length; j++) {
+                int curr = 0;
+                for(int k=i; k<=j; k++) {
+                    curr+=arr[k];
+                }
+                if (max < curr) {
+                    max = curr;
+                }
+            }
+        }
+        return max;
+    }
+}
