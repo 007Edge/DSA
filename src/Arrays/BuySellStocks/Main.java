a11d3d4907a23d832d6220416b9e6ec530795e45
007Edge <edge07.shiv@gmail.com>
2025-06-10 21:32:29 +0530

    trapping rain water

---
+++ src/Arrays/BuySellStocks/Main.java
@@ -0,0 +1,28 @@
+package Arrays.BuySellStocks;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] stock = {7, 1, 5, 3, 6, 4};
+        int max = profit(stock);
+        System.out.println(max);
+    }
+
+    public static int profit(int[] stock) {
+
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i: stock) {
+            if (buyPrice < i) {
+                int profit = i-buyPrice;
+                if (maxProfit < profit) {
+                    maxProfit = profit;
+                }
+            }
+            else {
+                buyPrice = i;
+            }
+        }
+        return maxProfit;
+    }
+}
