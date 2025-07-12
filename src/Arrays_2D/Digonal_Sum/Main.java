06380657e073ea4af292b5633dbfea20ddad4e13
007Edge <edge07.shiv@gmail.com>
2025-07-12 22:40:48 +0530

    Matrix (digonal sum)

---
+++ src/Arrays_2D/Digonal_Sum/Main.java
@@ -0,0 +1,35 @@
+package Arrays_2D.Digonal_Sum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,  2,  3},
+                {5,  6,  7},
+                {9,  10, 11}
+        };
+
+        printDigonalSum(matrix);
+
+    }
+    public static void printDigonalSum(int[][] matrix) {
+
+        int n = matrix.length-1;
+
+        int sum1 = 0;
+        for(int i=0; i<=n; i++) {
+            sum1 += matrix[i][i];
+        }
+
+        int sum2 = 0;
+        for(int i=0; i<=n; i++) {
+            if( matrix[i][i] == matrix[i][n-i] ) {     // test case => for nxn matrix where n is odd
+                continue;
+            }
+            sum2+= matrix[i][n-i];
+
+        }
+        System.out.println(sum1+sum2);
+
+    }
+
+}
