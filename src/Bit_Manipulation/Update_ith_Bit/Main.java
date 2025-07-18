4367a31b8218fdc8a5650282bcce0d3c4d582b23
007Edge <edge07.shiv@gmail.com>
2025-07-18 21:45:48 +0530

    Bit M (update ith bit 2nd method)

--- src/Bit_Manipulation/Update_ith_Bit/Main.java
+++ src/Bit_Manipulation/Update_ith_Bit/Main.java
@@ -2,10 +2,11 @@ package Bit_Manipulation.Update_ith_Bit;
 
 public class Main {
     public static void main(String[] args) {
-        int result = updateIthBit(9, 0, 0);
+        int result = updateIthBit(9, 2, 1);
         // updateIthBit(number, ithBit, set | clear)
 
         System.out.println(result);
+        System.out.println(update(9, 2, 1));
     }
 
     public static int updateIthBit(int n, int i, int bit) {
@@ -25,5 +26,12 @@ public class Main {
     public static int clearIthBit(int n, int i) {
         int bitMask = ~(1<<i);
         return bitMask & n;
+    }
+
+    public static int update(int n, int i, int bit) {
+        n = clearIthBit(n, i);
+        bit = bit << i;
+        return bit | n;
+
     }
 }
