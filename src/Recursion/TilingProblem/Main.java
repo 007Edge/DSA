0033603d326d33b61464d39226004b5dcc4bbc20
007Edge <edge07.shiv@gmail.com>
2025-07-28 23:58:37 +0530

    Recursion ( tiling problem for 2xn floor )

---
+++ src/Recursion/TilingProblem/Main.java
@@ -0,0 +1,16 @@
+package Recursion.TilingProblem;
+
+public class Main {
+    public static int tilingProblem(int n) {
+        if (n == 0 || n==1) {
+            return 1;
+        }
+        int fnm1 = tilingProblem(n-1);
+        int fnm2 = tilingProblem(n-2);
+
+        return fnm1+fnm2;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(4));
+    }
+}
